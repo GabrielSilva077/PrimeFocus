8b044820237cb2d48349f58e70a39c2de31829ec
GabrielSilva077 <felix.gabriel2027@gmail.com>
2025-10-14 10:46:14 -0300

    criando o form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,31 +1,51 @@
 import { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import Card from "../components/Card";
 import images from "../components/images";
 import { useGSAP } from "@gsap/react";
-import "../layout/contact.css"
+import "../layout/contact.css";
 
 const Contact = () => {
+
+  const mineNavRef = useRef(null);
+  const cardCardapioRef = useRef(null);
+
+  // Animação com useGSAP
+  useGSAP(() => {
+    const tl = gsap.timeline();
+
+    tl.fromTo(
+        mineNavRef.current,
+        { y: 200, opacity: 0 },
+        { y: 0, opacity: 1, duration: 0.7 }
+      )
+      .fromTo(
+        cardCardapioRef.current,
+        { y: 200, opacity: 0 },
+        { y: 0, opacity: 1, duration: 0.5 },
+      );
+  }, []);
+
   return (
-    <div id="paiContact">
-      <h1>Entre em Contato</h1>
-      <h1>Entre em Contato</h1>
-      <h1>Entre em Contato</h1>
-      <h1>Entre em Contato</h1>
-      <h1>Entre em Contato</h1>
-      <h1>Entre em Contato</h1>
-      <h1>Entre em Contato</h1>
-      <h1>Entre em Contato</h1>
-      <h1>Entre em Contato</h1>
-      <h1>Entre em Contato</h1>
-      <h1>Entre em Contato</h1>
-      <h1>Entre em Contato</h1>
-      <h1>Entre em Contato</h1>
-      <h1>Entre em Contato</h1>
-      <h1>Entre em Contato</h1>
-      <h1>Entre em Contato</h1>
-    </div>
+    <section id="paiContact">
+      <div className="alinhamento">
+        <h1 className="titleContact" ref={mineNavRef}>Entre em Contato</h1>
+
+        <div className="divForm" ref={cardCardapioRef}>
+          <img src={images[23]} alt="" className="imgContact" />
+          <form action="" className="form">
+            <input type="text" placeholder="Name" />
+            <input type="text" placeholder="Email" />
+            <textarea placeholder="Message" className="message"></textarea>
+            <input
+              type="button"
+              value="Enviar Mensagem"
+              className="btnEnviar"
+            />
+          </form>
+        </div>
+      </div>
+    </section>
   );
 };
 
